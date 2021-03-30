b4e5b4c0f2e0a24527667bb91f65f92ae437f732
C.T. Bell <charlestbell@gmail.com>
2021-03-29 21:21:53 -0400

    Removing the filter by button that never worked right

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import API from "./utils/API";
 import "./components/Table";
 import Table from "./components/Table";
@@ -10,12 +13,13 @@ function App() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState({});
   const [filter, setFilter] = useState("");
+  const [filterColumn, setFilterColumn] = useState("first name");
 
+  // Get users to populate the page
   useEffect(() => {
     API.getUsers()
       .then((res) => {
         setUsers(res.data.results);
-        // setTimeout(console.log(users), 300);
       })
       .catch((err) => setError({ error: err.message }));
   }, []);
@@ -25,8 +29,9 @@ function App() {
     setFilter(event.target.value);
   };
 
+  // Sort users by column
   const sortBy = (event) => {
-    console.log(event.target.dataset.column);
+    event.preventDefault();
     let localUsers;
     switch (event.target.dataset.column) {
       case "firstName":
@@ -96,20 +101,51 @@ function App() {
     }
     setUsers([...localUsers]);
   };
+
+  // Update state when user types in filter field
+  const handleFilterChange = (event) => {
+    setFilter("");
+    setFilterColumn(event.target.dataset.filter);
+  };
+
+  // Filter the users by what's in the filter field
   const filterBy = (event) => {
     event.preventDefault();
+    let localUsers;
+    switch (filterColumn) {
+      case "first name":
+        localUsers = users.filter((user) => user.name.first === filter);
+        break;
+      case "last name":
+        localUsers = users.filter((user) => user.name.last === filter);
+        break;
+      case "email":
+        localUsers = users.filter((user) => user.email === filter);
+        break;
+      case "country":
+        localUsers = users.filter((user) => user.location.country === filter);
+        break;
+      case "city":
+        localUsers = users.filter((user) => user.location.city === filter);
+        break;
 
-    let localUsers = users.filter((user) => user.name.first === filter);
+      default:
+        break;
+    }
 
     setUsers([...localUsers]);
   };
-  // console.log(users);
-  // console.log("Filter state", filter);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Employees</p>
-        <FilterBy filterBy={filterBy} handleInputChange={handleInputChange} />
+        <FilterBy
+          filterBy={filterBy}
+          handleInputChange={handleInputChange}
+          filterColumn={filterColumn}
+          handleFilterChange={handleFilterChange}
+        />
         <Table users={users} sortBy={sortBy} />
       </header>
     </div>
--- src/components/FilterBy/index.js
+++ src/components/FilterBy/index.js
@@ -1,21 +1,76 @@
 import React from "react";
 
-// import "./style.css";
-
 function FilterBy(props) {
   return (
     <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Find by Name</label>
+      <div className="form-group">
+        <div className="dropdown mb-1">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Filter By
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a
+              className="dropdown-item"
+              href="#"
+              data-filter="first name"
+              onClick={props.handleFilterChange}
+            >
+              First Name
+            </a>
+            <a
+              className="dropdown-item"
+              href="#"
+              data-filter="last name"
+              onClick={props.handleFilterChange}
+            >
+              Last Name
+            </a>
+            <a
+              className="dropdown-item"
+              href="#"
+              data-filter="email"
+              onClick={props.handleFilterChange}
+            >
+              Email
+            </a>
+            <a
+              className="dropdown-item"
+              href="#"
+              data-filter="country"
+              onClick={props.handleFilterChange}
+            >
+              Country
+            </a>
+            <a
+              className="dropdown-item"
+              href="#"
+              data-filter="city"
+              onClick={props.handleFilterChange}
+            >
+              City
+            </a>
+          </div>
+        </div>
         <input
           type="text"
-          class="form-control"
-          placeholder="Enter name"
+          className="form-control"
+          placeholder={"Enter " + props.filterColumn}
           onChange={props.handleInputChange}
         ></input>
       </div>
 
-      <button type="submit" class="btn btn-light" onClick={props.filterBy}>
+      <button
+        type="submit"
+        className="btn btn-light mx-1"
+        onClick={props.filterBy}
+      >
         Filter
       </button>
     </form>
